89bf2f068f0f15610b64fc2244d6b090ce3b6654
robohen <88636103+robohen@users.noreply.github.com>
2023-05-26 08:13:23 -0700

    some algos

--- loops.js
+++ loops.js
@@ -34,8 +34,50 @@
 //   }
 
 //Output an increasing number of # symbols, from 1 to 7
-let output = " ";
-for (let i = 1; i <= 7; i++) {
-    output += "#";
-    console.log(output);
-}
+// let output = " ";
+// for (let i = 1; i <= 7; i++) {
+//     output += "#";
+//     console.log(output);
+// }
+// let a = 10;
+// while (a >= 0){
+//     console.log(a);
+//     a--;
+// }
+// let i = 3;
+
+// while (i <= 35) {
+//   console.log(i);
+//   i += 3;
+// }
+// let a = 1;
+
+// while (a < 100) {
+//   console.log(a);
+//   a*=5
+// }
+// let a = 0;
+// while (a < 20) {
+//   if(a % 2 ===0){
+//     console.log(a * 3);
+//   }
+//   a++;
+// }
+// let a = 0;
+// while (a < 20) {
+//   if(a % 2 !== 0){
+//     console.log(a);
+//   }
+//   a++;
+// }
+// const bill = 10;
+// const price = 4;
+
+// let change = bill - price;
+// let q = 0;
+
+// while (change >= 0.25) {
+//     q++;
+//     change -= 0.25;
+// }
+// console.log(q);
---
+++ practice.js
@@ -0,0 +1,24 @@
+// string = "Hello World";
+
+// string2 = "";
+
+// for (i=string.length-1; i>=0; i--) {
+//    string2 += string[i];
+//    console.log(string2);
+// }
+// console.log(string2);
+
+// let counter = 0;
+//     for (let i = 1; i <= 100; i++) {
+//     counter+= i;
+// }
+
+
+// console.log(counter);
+
+function table(number) {
+    for (let i = 1; i <= 10; i++) {
+      let result = number * i;
+      console.log(number + " x " + i + " = " + result);
+    }
+  }
